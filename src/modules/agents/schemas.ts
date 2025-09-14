31e8365154c01e9b57fee737b0f00c2f41531c3f
Saurabh Pandey <ampsaurabh88877@gmail.com>
2025-09-14 22:56:59 +0530

    update and delete procedure

--- src/modules/agents/schemas.ts
+++ src/modules/agents/schemas.ts
@@ -3,3 +3,8 @@ export const agentCreateSchema=z.object({
     name: z.string().min(1, {message: 'Name is required'}),
     instruction:z.string().min(1, {message: 'Instruction is required'}),
 });
+
+export const agentsUpdateSchema=  agentCreateSchema.extend({
+    id:z.string().min(1,{message: "Id is required"}),
+
+})
